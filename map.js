f2b56e3b541767c320b6c161b1e5c2b974ddf3f1
Abhinav Cherukat <113844613+Abhi67981@users.noreply.github.com>
2024-07-25 16:05:33 +0100

    Update map.js
    Added additonal features

--- map.js
+++ map.js
@@ -25,8 +25,68 @@ function setupLayer() {
     // Center coordinates are defined in EPSG:3857 lon/lat and we are asking for srs=3857 in the "transformRequest"
     var serviceUrl = "https://api.os.uk/maps/vector/v1/vts"
     var map = L.map('map', {
-      maxZoom: 20
+      maxZoom: 20,
+      minZoom: 7
     }).setView([53.2934429, -2.4784419], 9);
+    L.control.browserPrint().addTo(map);
+    
+    var drawnFeatures = new L.FeatureGroup();
+    map.addLayer(drawnFeatures);
+
+    var drawControl = new L.Control.Draw({
+        position: 'topright',
+        edit: {
+            featureGroup: drawnFeatures,
+            remove: true
+        },
+        draw: {
+		    polygon: {
+		     shapeOptions: {
+		      color: 'purple'
+		     },
+		     allowIntersection: false,
+		     drawError: {
+		      color: 'orange',
+		      timeout: 1000
+		     },
+		    },
+		    polyline: {
+		     shapeOptions: {
+		      color: 'red'
+		     },
+		    },
+		    rect: {
+		     shapeOptions: {
+		      color: 'green'
+		     },
+		    },
+		    circle: {
+		     shapeOptions: {
+		      color: 'steelblue'
+            }
+        }
+        }
+    });
+    map.addControl(drawControl);
+ 
+    
+    map.on("draw:created", function(e){
+        var type = e.layerType;
+        var layer = e.layer;
+        console.log(layer)
+        drawnFeatures.addLayer(layer);
+    });
+    
+    map.on("draw:edited", function(e){
+        var layers = e.layers;
+        var type = e.layerType;
+
+        layers.eachLayer(function(layer){
+            console.log(layer)
+        })
+    });
+
+
     var gl = L.mapboxGL({
         accessToken: 'no-token',
         style: serviceUrl + '/resources/styles',
@@ -40,11 +100,13 @@ function setupLayer() {
             }
         }
     }).addTo(map);
-    
+
+
+
 
     map.attributionControl.addAttribution('&copy; <a href="http://www.ordnancesurvey.co.uk/">Ordnance Survey</a>');
     map.attributionControl.addAttribution('&copy; <a href="http://www.fgplimited.co.uk/">FGP Surevyors Limited</a>');
-    L.control.browserPrint().addTo(map);
+    
     gl.getMapboxMap().on('error', error => {
         console.log(error);
         message.classList.add("warning");
@@ -52,4 +114,5 @@ function setupLayer() {
         instructions.classList.remove("hidden");
     });
 }
+
 
